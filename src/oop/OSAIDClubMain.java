a76606b2824c8b13f86b850e07be838dc647a94d
Adsana Mohendran <adsana.mohendran21@ycdsbk12.ca>
2021-01-06 19:52:35 +0000

    added students and teachers to main class

--- src/oop/OSAIDClubMain.java
+++ src/oop/OSAIDClubMain.java
@@ -5,6 +5,18 @@ import oop.*;
 public class OSAIDClubMain{
 
   public static void main(String[] args){
-    Student Sarah = new Student("Sarah", "Rogers", )
+    Student Sarah = new Student("Sarah", "Rogers", "Girl", "online pledge", 10,new Position("General","make proposal") );
+
+    Teacher Smith = new Teacher("Tony", "Smith", "Boy", "Holiday Bingo", "Chemistry" );
+
+    Initiative VirtualGame = new Initiative("Virtual Game", "September", 4);
+
+    System.out.println(Sarah.getFirstName());
+    System.out.println(Smith.getLastName());
+    System.println(VirtualGame.getEvent());
+
+    OSAIDClub BringAwareness = new OSAIDClub(BringAwareness, Smith);
+    BringAwareness.addStudent(Sarah);
+
   }
 }
